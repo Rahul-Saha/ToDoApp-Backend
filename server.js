db07fff58110f4fdc3dc5cdc2c9b15afd8b17205
Rahul Saha <rahulsaha1996@gmail.com>
2022-03-11 10:28:17 +0530

    fix: refactor hard coded connection string to Heroku config var

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = require('./app');
 const port = process.env.PORT || 8080;
 // const port = 5000;
 
+const uri = process.env.MONGODB_URI;
+
 const server = http.createServer(app);
 
 server.listen(
